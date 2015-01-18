3df8f587b1d085005e0b122a1b4bc46a2cc9f69a
Martin Turjak <martinturjak@gmail.com>
2015-01-17 20:31:33 -0800

    defactoring middleware

--- application/libs/Middleware/PostJSON.php
+++ application/libs/Middleware/PostJSON.php
@@ -12,26 +12,28 @@ class PostJSON extends \Slim\Middleware
      */
     public function call()
     {
-        $req = $this->app->request();
+        $app = $this->app;
+        $req = $app->request();
 
-        if($this->app->response_type === 'api' && $req->isPost()) {
+        if($app->response_type === 'api' && $req->isPost()) {
             $body = json_decode($req->getBody());
 
             // checks if valid json format
             if(json_last_error() === JSON_ERROR_NONE) {
-              $env = $this->app->environment();
+              $env = $app->environment();
               $env['slim.request.form_hash'] = (array)$body;
+
+              // continue
+              $this->next->call();
             } else {
               $error = (object)array(
                   'error' => true,
                   'message' => 'Error in submited data! Not valid JSON format.'
               );
+              $app->contentType('application/json');
               $app->response->setStatus(400);
-              // we have to setBody explicitely
-              echo(json_encode($error));
+              $app->response->setBody(json_encode($error));
             }
         }
-
-        $this->next->call();
     }
 }
